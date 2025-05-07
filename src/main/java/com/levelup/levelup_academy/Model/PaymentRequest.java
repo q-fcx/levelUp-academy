64de517dbe333e12561af60242830a3b853b8687
Khaled <khaledalshhry1464@gmail.com>
2025-05-07 17:44:15 +0300

    notification and payment

--- src/main/java/com/levelup/levelup_academy/Model/PaymentRequest.java
+++ src/main/java/com/levelup/levelup_academy/Model/PaymentRequest.java
@@ -3,7 +3,7 @@ package com.levelup.levelup_academy.Model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
+// comment
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
